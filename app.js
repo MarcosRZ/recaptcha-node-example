58d9a41b742483383e6a93b716682f6c07a4fa8d
MarcosRZ <msucaro@mail.com>
2017-07-14 10:37:10 +0200

    Added json parsing to request body

--- app.js
+++ app.js
@@ -32,10 +32,9 @@ app.post('/contact', (req, res) => {
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body)
-                if (body.success){
+                if (JSON.parse(body).success){
                     res.send('<h1>Congratulations, ' + req.body.name + '! It seems that you\'re not a robot! :)</h1>')
                 } else {
-
                     res.send('<h1>Damn, ' + req.body.name + '! It seems that you\'re a robot! :(</h1>')
                 }
                 
