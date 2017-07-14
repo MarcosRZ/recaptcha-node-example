61cc96ff51bbe17c0cee7932f191825c0bd20fbc
MarcosRZ <msucaro@mail.com>
2017-07-14 09:57:09 +0200

    Added vhost code

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.post('/contact', (req, res) => {
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+module.exports = app
